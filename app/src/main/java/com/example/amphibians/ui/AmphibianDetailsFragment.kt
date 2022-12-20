caf7506b3ba3a58bc5ab72530ad1a61ea1786e93
prasad-more-12 <prsadmore012@gmail.com>
2022-12-20 13:13:30 +0530

    amphibian details added

--- app/src/main/java/com/example/amphibians/ui/AmphibianDetailsFragment.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianDetailsFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.amphibians.R
+import com.example.amphibians.databinding.FragmentAmphibianDetailsBinding
 
 class AmphibianDetailsFragment : Fragment() {
-
+    val viewModel: AmphibianViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_amphibian_details, container, false)
+        val binding = FragmentAmphibianDetailsBinding.inflate(inflater)
+        binding.lifecycleOwner=this
+        binding.viewModel=viewModel
+        return binding.root
     }
 
 
